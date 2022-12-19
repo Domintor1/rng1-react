761d93424ce3c68f1a0468fcc60fec64465b48f3
Ahmed <a.dirir90@gmail.com>
2022-12-19 16:57:43 +0000

    styling complete

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles.css';
 import reportWebVitals from './reportWebVitals';
 
-const Test = function(){
-  return(
-    <React.Fragment>
-  <div>
-  <button> Press for happiness </button>
-  <h1> Test for react application now</h1>
-  </div>
-  </React.Fragment>
+
+
+
+const RNG = function () {
+  return (
+    <div className='hero'>
+      <div className='container'>
+        <div className='randomNum'>
+          <p>
+            Random number: <span> 5</span>
+          </p>
+        </div>
+        <div className='numContainer'>
+          <div>
+            <p>Min:</p>
+            <input type='number' />
+          </div>
+          <div>
+            <p>Min:</p>
+            <input type='number' />
+          </div>
+        </div>
+        <button>Get Random Number</button>
+      </div>
+    </div>
   )
 }
-
-
 
 
 // If you want to start measuring performance in your app, pass a function
@@ -25,6 +39,6 @@ reportWebVitals();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Test />
+    <RNG />
   </React.StrictMode>
 );
